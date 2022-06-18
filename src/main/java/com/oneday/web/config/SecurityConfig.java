564b0750070d533f13292301e469c1b7089a483c
kmj <znr119@naver.com>
2022-06-18 16:07:03 +0900

    [2022-06-18 김민정] SecurityFilterChain 빈 등록하는 방식으로 변경

--- src/main/java/com/oneday/web/config/SecurityConfig.java
+++ src/main/java/com/oneday/web/config/SecurityConfig.java
@@ -3,31 +3,50 @@ package com.oneday.web.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 @Log4j2
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+      UserDetails user = User.builder().username("kmj")
+              .password("$2a$10$ZDFtW4pl8g.X93NgxbM27.ehqj9MRnYjr.8hZnIZsmZuNMCWuKafS")
+              .roles("USER")
+              .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    // [2022-06-18 김민정 수정 Start]
+    /* WebSecurityConfigurerAdapter deprecated 이유로 수정
+    @Bean
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         // 사용자 계정 kmj
         auth.inMemoryAuthentication().withUser("kmj")
                 // pw 1111 인코딩한 결과 > $2a$10$ZDFtW4pl8g.X93NgxbM27.ehqj9MRnYjr.8hZnIZsmZuNMCWuKafS
                 .password("$2a$10$ZDFtW4pl8g.X93NgxbM27.ehqj9MRnYjr.8hZnIZsmZuNMCWuKafS")
                 .roles("USER");
     }
+    */
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    // WebSecurityConfigurerAdapter > SecurityFilterChain 빈 등록 방식으로 변경
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/sample/all").permitAll()
                 .antMatchers("/sample/ex*").permitAll()
@@ -37,5 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable(); // csrf 토큰 비활성화
         http.logout(); // invalidatedHttpSession() deleteCookies() 쿠키나 세션을 무효화 시키는 설정 추가 가능
 
+        return http.build();
     }
+    // [2022-06-18 김민정 수정 End]
 }
