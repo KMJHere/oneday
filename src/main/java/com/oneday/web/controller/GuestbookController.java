c7be77982d9cb5ce8442e5375f4bdde898ea85e3
kmj <znr119@naver.com>
2022-07-02 17:13:41 +0900

    [2022-07-02 김민정] 페이징처리 화면 연동 TEST

--- src/main/java/com/oneday/web/controller/GuestbookController.java
+++ src/main/java/com/oneday/web/controller/GuestbookController.java
@@ -1,18 +1,30 @@
 package com.oneday.web.controller;
 
+import com.oneday.web.dto.PageRequestDTO;
+import com.oneday.web.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor // 자동 주입을 위한 annotaion
 public class GuestbookController {
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list..");
+    private final GuestbookService guestbookService; // final로 선언
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String index() {
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        log.info("본격적인...후후" + pageRequestDTO);
+
+        model.addAttribute("result", guestbookService.getList(pageRequestDTO));
     }
 }
