b0ffdd7a74f700f39408aff88eccc2b2a3f27982
kmj <znr119@naver.com>
2022-09-08 11:52:06 +0900

    [2022-09-08 김민정] thumbnail 생성 로직 추가

--- src/main/java/com/oneday/web/controller/UploadController.java
+++ src/main/java/com/oneday/web/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.oneday.web.controller;
 
 import com.oneday.web.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import net.coobird.thumbnailator.*;
 
 @RestController
 @Log4j2
@@ -62,7 +64,17 @@ public class UploadController {
             Path savePath = Paths.get(saveName);
 
             try {
+                // 원본 파일 저장
                 uploadFile.transferTo(savePath);
+
+                // 섬네일 생성
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+
+                File thumbnailFile = new File(thumbnailSaveName);
+
+                // 썸네일 생성 > Thumbnailator 라이브러리 이용
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
                 resultDTOList.add(new UploadResultDTO(fileName, uuid, folderPath));
             } catch(IOException e) {
                 e.printStackTrace();
