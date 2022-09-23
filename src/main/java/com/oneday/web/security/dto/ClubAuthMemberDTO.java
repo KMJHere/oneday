6bebeb66228c01d6ca1d303ea42f5d13d7b98726
kmj <znr119@naver.com>
2022-09-23 17:53:04 +0900

    [2022-09-23 김민정] 구글 소셜로그인 구현

--- src/main/java/com/oneday/web/security/dto/ClubAuthMemberDTO.java
+++ src/main/java/com/oneday/web/security/dto/ClubAuthMemberDTO.java
@@ -6,14 +6,16 @@ import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Log4j2
 @Getter
 @Setter
 @ToString
-public class ClubAuthMemberDTO extends User {
+public class ClubAuthMemberDTO extends User implements OAuth2User {
     /* User 클래스를 상속 후 생성자 호출
     public ClubAuthMemberDTO(String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
@@ -23,13 +25,28 @@ public class ClubAuthMemberDTO extends User {
     // DTO와 유사하게 구성(DTO 역할을 수행하는 클래스이며 Spring Security 인가/인증 작업에 사용할 수 있음)
     private String email;
 
+    private String password;
+
     private String name;
 
     private boolean fromSocial;
 
+    private Map<String, Object> attr;
+
+    public ClubAuthMemberDTO(String username, String password, boolean fromSocial, Collection<? extends GrantedAuthority> authorities, Map<String, Object> attr ) {
+        this(username, password, fromSocial, authorities);
+        this.attr = attr;
+    }
+
     public ClubAuthMemberDTO(String username, String password, boolean fromSocial, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         this.email = username;
+        this.password = password;
         this.fromSocial = fromSocial;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return this.attr;
     }
 }
